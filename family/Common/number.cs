6d75eaf7a86a2d69ed05b254beef0af74ea7b948
shenxy <yalunwang123@163.com>
2015-09-09 23:36:40 +0800

    提交未跟踪的文件

---
+++ family/Common/number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZKEShop.Common
+{
+    public  class number
+    {
+        /// <summary>
+        /// 将decimal？转为小数点只有两位
+        /// </summary>
+        /// <returns></returns>
+        public static decimal convTwo(decimal? moneywait )
+        {
+            decimal money;
+            money = Convert.ToDecimal(moneywait);
+            money = Math.Round(money,2);
+            return money;
+        }
+    }
+}
---
+++ jquery-easyui/Layout/adduser.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZKEShop.Common;
+namespace jquery_easyui.Layout
+{
+    public partial class adduser : pagebase
+    {
+        public IQueryable<DataAccess.Usergroup> usergrouplist;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string operation = Request["operation"];
+            switch (operation)
+            {
+                case "save":
+                    save();
+                    break;
+
+            }
+            load();
+
+        }
+        protected void load()
+        {
+             usergrouplist = from s in db.Usergroup select s;
+        
+        }
+        protected void save()
+        {
+
+            DataAccess.user usermodel = new DataAccess.user();
+            usermodel.UserName = ZKRequest.GetString("userName");
+            usermodel.LogDate = DateTime.Now;
+            usermodel.LastActive =DateTime.Now;
+            usermodel.isActive = true;
+            usermodel.UserPwd = ZKRequest.GetString("userPwd");
+            usermodel.Remark = ZKRequest.GetString("remark");
+            usermodel.UsergroupID = ZKRequest.GetInt("usergroupID",0);
+            db.user.InsertOnSubmit(usermodel);
+            db.SubmitChanges();
+            Response.Write("<script>alert('保存成功')</script>");
+
+        }
+    }
+}
